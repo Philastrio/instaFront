53598da6a95bd7e359e69d1e0328558669854716
HanKang <han_gang@naver.com>
2019-05-12 20:24:51 +0900

    React Router Complete

---
+++ src/Styles/Theme.js
@@ -0,0 +1,17 @@
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  blueColor: "#3897f0",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  darkBlueColor: "#003569",
+  redColor: "#ED4956",
+  whiteBox: `
+    ${BORDER_RADIUS};
+    ${BOX_BORDER}
+    background-color: white;
+  `
+};
