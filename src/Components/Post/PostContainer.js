c0c2eb4ba6cb3e8108343a1dae52a55a4967d7e4
HanKang <han_gang@naver.com>
2019-05-26 15:25:01 +0900

    7.1 Post component One complete!

---
+++ src/Components/Post/PostContainer.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import PostPresenter from "./PostPresenter";
+import useInput from "../../Hooks/useInput";
+
+const PostContainer = ({
+  id,
+  user,
+  files,
+  likeCount,
+  isLiked,
+  comments,
+  createdAt
+}) => {
+  return <PostPresenter />;
+};
+PostContainer.prototype = {
+  id: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
+    name: PropTypes.string.isRequired
+  }).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  likeCount: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  comments: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired
+      }).isRequired
+    })
+  ).isRequired,
+  createdA: PropTypes.string
+};
+
+export default PostContainer;
