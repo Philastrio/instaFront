18b50a4e74c7f02a193bf1769490a24b04f036b7
HanKang <han_gang@naver.com>
2019-06-03 18:36:58 +0900

    8.2 Complete

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -10,7 +10,7 @@ export default withRouter(({ location: { search } }) => {
     skip: term === undefined,
     variables: { term }
   });
-  console.log(data);
+
   return <SearchPresenter searchTerm={term} loading={loading} data={data} />;
 });
 
