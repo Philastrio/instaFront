15b059cc6d693cc6f1d8e45fb7a9a005e909b6a1
HanKang <han_gang@naver.com>
2019-06-05 06:34:45 +0900

    9.1 4:00 부터

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -2,14 +2,33 @@ import React from "react";
 import styled from "styled-components";
 import Loader from "../../Components/Loader";
 import Avatar from "../../Components/Avatar";
+import FatText from "../../Components/FatText";
 
 const Wrapper = styled.div`
   min-height: 60vh;
 `;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const HeaderColumn = styled.div``;
+
+const Username = styled.span`
+  font-size: 25px;
+  margin-bottom: 10px;
+  display: block;
+`;
+
+const Counts = styled.ul`
+  display: flex;
+`;
+
+const Count = styled.li``;
+
+const Bio = styled.p``;
 
 export default ({ loading, data }) => {
   if (loading === true) {
@@ -39,6 +58,22 @@ export default ({ loading, data }) => {
         <Header>
           <HeaderColumn>
             <Avatar size="lg" url={avatar} />
+          </HeaderColumn>
+          <HeaderColumn>
+            <Username>{name}</Username>
+            <Counts>
+              <Count>
+                <FatText text={String(postsCount)} /> 게시물
+              </Count>
+              <Count>
+                <FatText text={String(followersCount)} /> 팔로워
+              </Count>
+              <Count>
+                <FatText text={String(followingCount)} /> 팔로우
+              </Count>
+            </Counts>
+            <FatText text={fullName} />
+            <Bio>{bio}</Bio>
           </HeaderColumn>
         </Header>
       </>
