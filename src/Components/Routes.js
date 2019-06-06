69f4f7aa313c1fcb66447582def8dc944c33c64d
HanKang <han_gang@naver.com>
2019-06-07 08:18:00 +0900

    Instaclone Complete

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "../Routes/Auth";
 import Feed from "../Routes/Feed";
 import Explore from "../Routes/Explore";
@@ -14,12 +14,14 @@ const LoggedInRoutes = () => (
     <Route path="/explore" component={Explore} />
     <Route path="/search" component={Search} />
     <Route path="/:name" component={Profile} />
+    <Redirect from="*" to="/" />
   </Switch>
 );
 
 const LoggedOutRoutes = () => (
   <Switch>
     <Route exact path="/" component={Auth} />
+    <Redirect from="*" to="/" />
   </Switch>
 );
 
--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -1,36 +1,69 @@
 import React from "react";
 import styled from "styled-components";
+import { Helmet } from "rl-react-helmet";
 import Loader from "../../Components/Loader";
 import Avatar from "../../Components/Avatar";
 import FatText from "../../Components/FatText";
+import FollowButton from "../../Components/FollowButton";
+import SquarePost from "../../Components/SquarePost";
+import Button from "../../Components/Button";
 
 const Wrapper = styled.div`
-  min-height: 60vh;
+  min-height: 100vh;
 `;
 
 const Header = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
+  width: 80%;
+  margin: 0 auto;
+  margin-bottom: 50px;
 `;
 
 const HeaderColumn = styled.div``;
 
+const UsernameRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const Username = styled.span`
   font-size: 25px;
-  margin-bottom: 10px;
+
   display: block;
 `;
 
 const Counts = styled.ul`
   display: flex;
+  margin: 15px 0px;
 `;
 
-const Count = styled.li``;
+const Count = styled.li`
+  font-size: 16px;
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
 
-const Bio = styled.p``;
+const FullName = styled(FatText)`
+  font-size: 15px;
+`;
 
-export default ({ loading, data }) => {
+const Bio = styled.p`
+  margin: 10px 0px;
+`;
+
+const Posts = styled.div`
+  display: grid;
+  grid-gap: 25px;
+
+  grid-template-columns: repeat(4, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
+`;
+
+export default ({ loading, data, logOut }) => {
   if (loading === true) {
     return (
       <Wrapper>
@@ -54,13 +87,23 @@ export default ({ loading, data }) => {
       }
     } = data;
     return (
-      <>
+      <Wrapper>
+        <Helmet>
+          <title>{name} | InstaClone</title>
+        </Helmet>
         <Header>
           <HeaderColumn>
             <Avatar size="lg" url={avatar} />
           </HeaderColumn>
           <HeaderColumn>
-            <Username>{name}</Username>
+            <UsernameRow>
+              <Username>{name}</Username>
+              {isSelf ? (
+                <Button onClick={logOut} text="로그아웃" />
+              ) : (
+                <FollowButton id={id} isFollowing={isFollowing} />
+              )}
+            </UsernameRow>
             <Counts>
               <Count>
                 <FatText text={String(postsCount)} /> 게시물
@@ -72,11 +115,22 @@ export default ({ loading, data }) => {
                 <FatText text={String(followingCount)} /> 팔로우
               </Count>
             </Counts>
-            <FatText text={fullName} />
+            <FullName text={fullName} />
             <Bio>{bio}</Bio>
           </HeaderColumn>
         </Header>
-      </>
+        <Posts>
+          {posts &&
+            posts.map(post => (
+              <SquarePost
+                key={post.id}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                file={post.files[0]}
+              />
+            ))}
+        </Posts>
+      </Wrapper>
     );
   }
   return null;
